a4a9db1cf862a358b2992f0856cd6d01993f87c1
Nils Jaekel <nils.jaekel@davils.com>
2026-04-09 19:07:09 +0200

    feat: enhance ProjectModule to configure common project settings and apply default Gradle plugins

--- kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/Module.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/Module.kt
@@ -2,10 +2,22 @@ package com.davils.kreate.module.project
 
 import com.davils.kreate.KreateExtension
 import com.davils.kreate.module.Module
+import com.davils.kreate.module.project.plugin.applyDefaultGradlePlugins
 import org.gradle.api.Project
 
 internal object ProjectModule : Module {
     override fun apply(project: Project, extension: KreateExtension) {
+        project.configureCommon(extension)
+    }
 
+    private fun Project.configureCommon(extension: KreateExtension) {
+        applyDefaultGradlePlugins()
+        addRepositories()
+        configureGroup()
+        configureVersion()
+
+        afterEvaluate {
+            initializeProject(extension.project)
+        }
     }
 }
