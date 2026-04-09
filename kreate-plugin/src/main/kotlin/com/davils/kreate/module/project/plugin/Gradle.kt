fca1849202620c8e41e588d87e2f81586a67dbfa
Nils Jaekel <nils.jaekel@davils.com>
2026-04-09 19:07:19 +0200

    feat: add applyDefaultGradlePlugins function to apply SerializationGradleSubplugin

---
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/plugin/Gradle.kt
@@ -0,0 +1,9 @@
+package com.davils.kreate.module.project.plugin
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.jetbrains.kotlinx.serialization.gradle.SerializationGradleSubplugin
+
+internal fun Project.applyDefaultGradlePlugins() {
+    pluginManager.apply(SerializationGradleSubplugin::class)
+}
