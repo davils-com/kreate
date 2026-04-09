16b3eb449d38f95393fd8919cf137e49251c27c1
Nils Jaekel <nils.jaekel@davils.com>
2026-04-09 19:07:24 +0200

    feat: refactor ProjectExtension to use ObjectFactory for property management

--- kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/Extension.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/Extension.kt
@@ -1,5 +1,10 @@
 package com.davils.kreate.module.project
 
-public interface ProjectExtension {
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+import javax.inject.Inject
 
+public abstract class ProjectExtension @Inject constructor(factory: ObjectFactory) {
+    public val name: Property<String> = factory.property(String::class.java)
+    public val description: Property<String> = factory.property(String::class.java).convention("A Davils project.")
 }
