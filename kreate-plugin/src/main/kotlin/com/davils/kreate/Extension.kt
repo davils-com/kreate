62fe4ff130ec8cbe6d52a3bc60733fa80682bbeb
Nils Jaekel <nils.jaekel@davils.com>
2026-04-09 18:03:46 +0200

    feat: enhance KreateExtension with project and platform configuration methods

--- kreate-plugin/src/main/kotlin/com/davils/kreate/Extension.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/Extension.kt
@@ -1,5 +1,24 @@
 package com.davils.kreate
 
-public interface KreateExtension {
+import com.davils.kreate.module.platform.PlatformExtension
+import com.davils.kreate.module.project.ProjectExtension
+import org.gradle.api.Action
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.tasks.Nested
+import javax.inject.Inject
 
+public abstract class KreateExtension @Inject constructor(factory: ObjectFactory) {
+    @get:Nested
+    public abstract val project: ProjectExtension
+
+    @get:Nested
+    public abstract val platform: PlatformExtension
+
+    public fun project(action: Action<ProjectExtension>) {
+        action.execute(project)
+    }
+
+    public fun platform(action: Action<PlatformExtension>) {
+        action.execute(platform)
+    }
 }
