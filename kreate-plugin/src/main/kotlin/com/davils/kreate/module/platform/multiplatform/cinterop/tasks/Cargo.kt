2b95266f196dde3c3fa10de99304c0b39e57fcd9
Nils Jaekel <nils.jaekel@davils.com>
2026-04-10 19:25:49 +0200

    feat: add ConfigureCargo task for extending Cargo.toml with static library configuration

---
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/platform/multiplatform/cinterop/tasks/Cargo.kt
@@ -0,0 +1,50 @@
+package com.davils.kreate.module.platform.multiplatform.cinterop.tasks
+
+import com.davils.kreate.jobs.Task
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+public abstract class ConfigureCargo : Task("Configure Cargo Toml file for static library output.") {
+    @get:InputDirectory
+    public abstract val workDir: DirectoryProperty
+
+    private val extendedCargoContent: String
+        get() = """
+            [lib]
+            crate-type = ["staticlib"]
+        """.trimIndent()
+
+    @TaskAction
+    override fun execute() {
+        val cargoToml = workDir.get().asFile.resolve(CARGO_TOML_FILE_NAME)
+        if (!isValidCargoToml(cargoToml)) {
+            return
+        }
+
+        if (isContentAlreadyExtended(cargoToml)) {
+            return
+        }
+        writeContentToFile(cargoToml)
+    }
+
+    private fun isValidCargoToml(cargoToml: File): Boolean {
+        return cargoToml.exists()
+    }
+
+    private fun isContentAlreadyExtended(cargoToml: File): Boolean {
+        val content = cargoToml.readText()
+        return content.contains(extendedCargoContent)
+    }
+
+    private fun writeContentToFile(cargoToml: File) {
+        val originalContent = cargoToml.readText()
+        val newContent = originalContent + extendedCargoContent
+        cargoToml.writeText(newContent)
+    }
+
+    public companion object {
+        public const val CARGO_TOML_FILE_NAME: String = "Cargo.toml"
+    }
+}
