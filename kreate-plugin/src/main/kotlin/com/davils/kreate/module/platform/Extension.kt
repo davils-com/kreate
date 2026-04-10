02b7020aad3b2c0ad1ea0dfa13aae23c871d55b0
Nils Jaekel <nils.jaekel@davils.com>
2026-04-10 07:59:37 +0200

    feat: add javaVersion property to PlatformExtension with default to Java 25

--- kreate-plugin/src/main/kotlin/com/davils/kreate/module/platform/Extension.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/platform/Extension.kt
@@ -1,8 +1,10 @@
 package com.davils.kreate.module.platform
 
+import org.gradle.api.JavaVersion
 import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
 import javax.inject.Inject
 
 public abstract class PlatformExtension @Inject constructor(factory: ObjectFactory) {
-
+    public val javaVersion: Property<JavaVersion> = factory.property(JavaVersion::class.java).convention(JavaVersion.VERSION_25)
 }
