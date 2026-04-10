5ab87a37589539cf6b9331050d2bc18bb8cdddce
Nils Jaekel <nils.jaekel@davils.com>
2026-04-10 07:59:44 +0200

    feat: enhance Module.kt to support Kotlin Multiplatform and JVM configurations

--- kreate-plugin/src/main/kotlin/com/davils/kreate/module/platform/Module.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/platform/Module.kt
@@ -6,6 +6,26 @@ import org.gradle.api.Project
 
 internal object PlatformModule : Module {
     override fun apply(project: Project, extension: KreateExtension) {
+        project.plugins.withId("org.jetbrains.kotlin.multiplatform") {
+            project.configureCommon(extension)
+            project.configureMultiplatform(extension)
+        }
+
+        project.plugins.withId("org.jetbrains.kotlin.jvm") {
+            project.configureCommon(extension)
+            project.configureJvm(extension)
+        }
+    }
+
+    private fun Project.configureCommon(extension: KreateExtension) = afterEvaluate {
+        configureJava(extension)
+    }
+
+    private fun Project.configureJvm(extension: KreateExtension): Unit = afterEvaluate {
+
+    }
+
+    private fun Project.configureMultiplatform(extension: KreateExtension): Unit = afterEvaluate {
 
     }
 }
