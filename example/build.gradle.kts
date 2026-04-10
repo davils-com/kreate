d65a7320e154feaff0b216cdc51b6ebce4b12c58
Nils Jaekel <nils.jaekel@davils.com>
2026-04-10 11:22:43 +0200

    feat: update build.gradle.kts to configure Kotlin JVM version and add build constants

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,13 +1,26 @@
 plugins {
     alias(libs.plugins.kreate)
+    kotlin("jvm") version "2.3.20"
 }
 
 kreate {
     platform {
-
+        javaVersion = JavaVersion.VERSION_25
+        explicitApi = true
+        allWarningsAsErrors = false
     }
 
     project {
+        name = "Example"
+        description = "Example project"
 
+        buildConstant {
+            enabled = true
+            className = "ExampleConstants"
+            path = "generated/compile"
+
+            constant("example", "value")
+            constant("example2", 1)
+        }
     }
 }
