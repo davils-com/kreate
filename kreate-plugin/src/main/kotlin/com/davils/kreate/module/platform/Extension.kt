dfa41e10f2c85e7a1f1ed8412c585fe94ba01d0c
Nils Jaekel <nils.jaekel@davils.com>
2026-04-10 18:59:20 +0200

    feat: add multiplatform extension support in PlatformExtension class

--- kreate-plugin/src/main/kotlin/com/davils/kreate/module/platform/Extension.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/platform/Extension.kt
@@ -1,12 +1,22 @@
 package com.davils.kreate.module.platform
 
+import com.davils.kreate.module.platform.multiplatform.MultiplatformExtension
+import org.gradle.api.Action
 import org.gradle.api.JavaVersion
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Nested
 import javax.inject.Inject
 
 public abstract class PlatformExtension @Inject constructor(factory: ObjectFactory) {
     public val javaVersion: Property<JavaVersion> = factory.property(JavaVersion::class.java).convention(JavaVersion.VERSION_25)
     public val explicitApi: Property<Boolean> = factory.property(Boolean::class.java).convention(true)
     public val allWarningsAsErrors: Property<Boolean> = factory.property(Boolean::class.java).convention(false)
+
+    @get:Nested
+    public abstract val multiplatform: MultiplatformExtension
+
+    public fun multiplatform(action: Action<MultiplatformExtension>) {
+        action.execute(multiplatform)
+    }
 }
