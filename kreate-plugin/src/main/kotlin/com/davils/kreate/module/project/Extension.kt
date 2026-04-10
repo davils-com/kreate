90a3a80e6f091be323417f4e6081b5b21ce52bee
Nils Jaekel <nils.jaekel@davils.com>
2026-04-10 10:50:46 +0200

    feat: add BuildConstantsExtension and integrate with ProjectExtension for build configuration

--- kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/Extension.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/Extension.kt
@@ -1,10 +1,20 @@
 package com.davils.kreate.module.project
 
+import com.davils.kreate.module.project.constants.BuildConstantsExtension
+import org.gradle.api.Action
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Nested
 import javax.inject.Inject
 
 public abstract class ProjectExtension @Inject constructor(factory: ObjectFactory) {
     public val name: Property<String> = factory.property(String::class.java)
     public val description: Property<String> = factory.property(String::class.java).convention("A Davils project.")
+
+    @get:Nested
+    public abstract val buildConstants: BuildConstantsExtension
+
+    public fun buildConstant(action: Action<BuildConstantsExtension>) {
+        action.execute(buildConstants)
+    }
 }
---
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/constants/Extension.kt
@@ -0,0 +1,32 @@
+package com.davils.kreate.module.project.constants
+
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.MapProperty
+import org.gradle.api.provider.Property
+import javax.inject.Inject
+
+public abstract class BuildConstantsExtension @Inject constructor(factory: ObjectFactory) {
+    private val properties: MapProperty<String, String> = factory.mapProperty(String::class.java, String::class.java).convention(emptyMap())
+
+    public val enabled: Property<Boolean> = factory.property(Boolean::class.java).convention(false)
+
+    public val packageNameOverride: Property<String> = factory.property(String::class.java)
+    public val className: Property<String> = factory.property(String::class.java).convention("BuildConstants")
+    public val path: Property<String> = factory.property(String::class.java).convention("generated/compile")
+
+    public fun constant(key: String, value: String) {
+        if (key.isBlank()) {
+            throw IllegalArgumentException("Key cannot be blank.")
+        }
+
+        properties.put(key, value)
+    }
+
+    public fun constant(key: String, value: Any) {
+        constant(key, value.toString())
+    }
+
+    public fun getConstants(): Map<String, String> {
+        return properties.get().toMap()
+    }
+}
