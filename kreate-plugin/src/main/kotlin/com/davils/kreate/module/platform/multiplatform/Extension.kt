3eaa26a00c1baf34dd63bc82224bdd6d35eb31f4
Nils Jaekel <nils.jaekel@davils.com>
2026-04-10 18:59:45 +0200

    feat: add MultiplatformExtension class for Kotlin multiplatform support

---
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/platform/multiplatform/Extension.kt
@@ -0,0 +1,8 @@
+package com.davils.kreate.module.platform.multiplatform
+
+import org.gradle.api.model.ObjectFactory
+import javax.inject.Inject
+
+public abstract class MultiplatformExtension @Inject constructor(factory: ObjectFactory) {
+
+}
