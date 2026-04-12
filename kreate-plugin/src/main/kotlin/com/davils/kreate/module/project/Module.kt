16e78a4198c63a1976396c928dbb41b716efcc9a
Nils Jaekel <nils.jaekel@davils.com>
2026-04-12 13:48:09 +0200

    feat: add publishing initialization to module setup

--- kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/Module.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/Module.kt
@@ -4,6 +4,7 @@ import com.davils.kreate.KreateExtension
 import com.davils.kreate.module.Module
 import com.davils.kreate.module.project.constants.initializeBuildConstants
 import com.davils.kreate.module.project.docs.initializeDocs
+import com.davils.kreate.module.project.publish.initializePublish
 import com.davils.kreate.module.project.tests.initializeTesting
 import org.gradle.api.Project
 
@@ -23,6 +24,7 @@ internal object ProjectModule : Module {
             initializeBuildConstants(extension)
             initializeDocs(extension)
             initializeTesting(extension)
+            initializePublish(extension)
         }
     }
 }
