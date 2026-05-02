dc70ec98a2d5f4ede83e4604e0c904169aef1e20
Nils Jaekel <nils.jaekel@davils.com>
2026-05-02 13:40:22 +0200

    refactor!: remove plugin application calls and enforce manual application of plugins

--- kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/publish/Gitlab.kt
+++ kreate-plugin/src/main/kotlin/com/davils/kreate/module/project/publish/Gitlab.kt
@@ -20,7 +20,6 @@ import com.davils.kreate.KreateExtension
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.artifacts.repositories.PasswordCredentials
 import org.gradle.authentication.http.HttpHeaderAuthentication
 import org.gradle.kotlin.dsl.credentials
@@ -46,6 +45,10 @@ internal fun Project.configureGitlab(
     val tokenEnvName = gitlabConfig.tokenEnv.get()
     val jobToken = System.getenv(tokenEnvName)
 
+    if (!plugins.hasPlugin("maven-publish")) {
+        error("Maven Publish Plugin not applied. Do it yourself: 'maven-publish'")
+    }
+
     if (jobToken == null) {
         logger.lifecycle("No CI job token found in $tokenEnvName, skipping GitLab publish repository")
         return
@@ -54,8 +57,6 @@ internal fun Project.configureGitlab(
     val repoName = gitlabConfig.name.orNull ?: "GitlabPackageRegistry"
     val projectId = System.getenv(gitlabConfig.projectIdEnv.get())
     val apiV4 = System.getenv(gitlabConfig.apiUrlEnv.get())
-
-    pluginManager.apply(MavenPublishPlugin::class.java)
 
     extensions.configure<PublishingExtension>("publishing") {
         repositories {
